2a940c757bf711ae389ac99fe2a9ab0729e3a326
ryank <ryank@Alien>
2020-05-06 21:04:50 +0200

    Initialisation du microservice

---
+++ Application/src/main/java/cm/thesupermarket/application/AccountApplication.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cm.thesupermarket.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ *
+ * @author ryank
+ */
+
+@SpringBootApplication
+@EntityScan(basePackages = {"com.open.school.domain"})
+@EnableJpaRepositories(basePackages = {"com.open.school.repository"})
+@PropertySource("classpath:application.properties")
+@EnableScheduling
+public class AccountApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         SpringApplication.run(AccountApplication.class, args);
+    }
+    
+}
